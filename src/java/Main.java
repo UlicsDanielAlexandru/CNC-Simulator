861c34ac874b816f81c6ea8f398573db18fe6e23
UlicsDanielAlexandru <dani_ulics_2000@yahoo.com>
2021-12-09 20:51:52 +0200

    fixed main

--- src/java/Main.java
+++ src/java/Main.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class Main {
+import controller.Controller;
+import view.View;
+
+public class Main {
+
+    public static void main(String[] args) {
+        View view = new View();
+        Controller controller = new Controller(view);
+    }
 }
